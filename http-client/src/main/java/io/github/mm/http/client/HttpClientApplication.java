d0599175c6cc0f24b5c1405c9a58f445461d91c1
Matej Marconak <2461939+marconak-matej@users.noreply.github.com>
2025-10-18 12:42:50 +0200

    fix: change main method visibility to public in HttpClientApplication

--- http-client/src/main/java/io/github/mm/http/client/HttpClientApplication.java
+++ http-client/src/main/java/io/github/mm/http/client/HttpClientApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HttpClientApplication {
 
-    static void main(String[] args) {
+    public static void main(String[] args) {
         SpringApplication.run(HttpClientApplication.class, args);
     }
 }
